9c3c3e54c0beff7db07e655115cb53de7ab587db
Angelina F <angelina.friz@gmail.com>
2019-06-03 11:17:46 +0300

    Initial Commit

---
+++ Pirate Adventure Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventure Game
+//
+//  Created by Angelina on 6/3/19.
+//  Copyright © 2019 Angelina Friz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventure Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure Game
+//
+//  Created by Angelina on 6/3/19.
+//  Copyright © 2019 Angelina Friz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
